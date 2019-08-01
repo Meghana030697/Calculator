774bb90c0d76e55dae7a751bfcf1edfb80d955d8
Meghana Rajkumar <53405846+Meghana030697@users.noreply.github.com>
2019-08-01 10:06:45 +0530

    sbCalculator.java

---
+++ sbCalculator.java
@@ -0,0 +1,45 @@
+package com.epsilon.training.sb.calculator.bo;
+
+public class Calculator {
+
+	private int ip1, ip2;
+
+	public int getIp1() {
+		return ip1;
+	}
+
+	public void setIp1(int ip1) {
+		this.ip1 = ip1;
+	}
+
+	public int getIp2() {
+		return ip2;
+	}
+
+	public void setIp2(int ip2) {
+		this.ip2 = ip2;
+	}
+
+	public Calculator() {
+		//super();
+		System.out.println("Calculator instance is created");
+		// TODO Auto-generated constructor stub
+	}
+	public int add(int i, int j)
+	{
+		return i+j;
+	}
+	public int sub(int i, int j)
+	{
+		return i-j;
+	}
+	public int mul(int i, int j)
+	{
+		return i*j;
+	}
+	public int div(int i, int j)
+	{
+		return i/j;
+	}
+	
+}
